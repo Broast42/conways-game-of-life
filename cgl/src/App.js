968fd2ffa5160f58a03c570e3aef2082a24cd0a7
rdowd <rjarrettdowd@yahoo.com>
2020-07-23 22:18:06 -0400

    Did some inital work on game component created a function that creates a matrix array of 25 X 25

--- cgl/src/App.js
+++ cgl/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Game from './components/Game'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
     
-    </div>
+    <Game />
+    
   );
 }
 
---
+++ cgl/src/components/Game.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import '../App.css'
+
+function Game() {
+    //set the row and column heigh
+    const [rowLen, setRowLen] = useState(25)
+    const [columnLen, setColumnLen] = useState(25)
+
+    //set simulation speed 1000 = 1 second
+    const [simSpeed, setSimSpeed] = useState(1000)
+
+    //bg color -stretch
+    //cell color -stretch
+
+    //state to check if sim is running
+    const [isRunning, setIsRunning] = useState(false)
+
+    //individual neighbor indexes
+    const neighborAddress = [[0,-1],[-1, -1],[1, -1],[1, -1],[1, 0],[0, 1],[-1, 1],[1,1]]
+
+    //default grid array
+    const defaultGrid = () => {
+        const gridArr = []
+        //for each item in the row push an array the size of the column length with 0 values
+        for (let i = 0; i < rowLen; i++){
+            gridArr.push(Array.from(Array(columnLen), () => 0))
+        }
+        return gridArr
+    }
+
+    console.log(defaultGrid())
+
+    return (
+        <div >
+        
+        </div>
+    )
+}
+
+export default Game;
